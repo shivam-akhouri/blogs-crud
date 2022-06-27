771bfd25278d68c9576bd11319c5eed038b62b56
Shivam Akhouri <shivamakhouri1142@gmail.com>
2022-06-27 11:15:23 +0530

    added store.js content

---
+++ utils/store.js
@@ -0,0 +1,12 @@
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBynpO6M7k4MtVe7qJi0IYnB8-UQW-uxqM",
+    authDomain: "blogs-todo.firebaseapp.com",
+    projectId: "blogs-todo",
+    storageBucket: "blogs-todo.appspot.com",
+    messagingSenderId: "930176384533",
+    appId: "1:930176384533:web:8a72e4f44cd4bf46ac981c"
+  };
+
+const app = initializeApp(firebaseConfig);
